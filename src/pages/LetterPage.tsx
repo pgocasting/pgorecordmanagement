8cee41ad689a0388470543164f60f3a2e5c443f1
pgocasting <pgocasting@gmail.com>
2025-12-23 17:17:10 +0800

    123

--- src/pages/LetterPage.tsx
+++ src/pages/LetterPage.tsx
@@ -28,13 +28,6 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import {
   Plus,
   Menu,
   LogOut,
